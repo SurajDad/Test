8b3f1924bdf558149b124a57c67ec916030c0e82
SurajDad <80150336+SurajDad@users.noreply.github.com>
2021-10-25 23:11:32 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,66 @@
+print("Icon Steel")
+print("   ")
+
+rate = input("Today's Rate??? ")
+print('    ')
+TMT = int(rate) + int(3120) 
+TMT2 = int(TMT) * float(0.18)
+TMT3 = float(TMT2) + int(TMT)
+TMT4 = float(TMT3) * float(0.015)
+TMT5 = float(TMT3) - float(TMT4) 
+print("TMT 10mm to 25mm: ")
+print(TMT5)
+
+x = int(rate) + int(620)
+y = int(x) * float(0.18)
+z = int(x) + float(y)
+m = float(z) * float(0.015)
+n = float(z) - float(m)
+print("   ")
+print("TMT in random 10mm to 25mm")
+print(n)
+
+print("   ")
+a = int(rate) + int(4120) 
+b = int(a) * float(0.18)
+c = float(b) + int(a)
+d = float(c) * float(0.015)
+e = float(c) - float(d) 
+print("TMT 8mm: ")
+print(e)
+
+print('     ')
+xx = int(rate) + int(1620)
+yy = int(xx) * float(0.18)
+zz = int(xx) + float(yy)
+mm = float(zz) * float(0.015)
+nn = float(zz) - float(mm)
+print("   ")
+print("TMT in random 8mm")
+print(nn)
+
+print('    ')
+aa = int(rate) + int(6120) 
+bb = int(aa) * float(0.18)
+cc = float(bb) + int(aa)
+dd = float(cc) * float(0.015)
+ee = float(cc) - float(dd) 
+print("TMT 6mm: ")
+print(ee)
+
+print('     ')
+xxx = int(rate) + int(3620)
+yyy = int(xxx) * float(0.18)
+zzz = int(xxx) + float(yyy)
+mmm = float(zzz) * float(0.015)
+nnn = float(zzz) - float(mmm)
+print("   ")
+print("TMT in random 6mm")
+print(nnn)
+console.log("Yo")
+
+
+
+
+
+
